485b0317ca4453683150df6a9ab4644d1b8670e8
pranavramesh123 <70009469+pranavramesh123@users.noreply.github.com>
2020-08-21 16:49:05 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,28 @@
+import wolframalpha
+import pyttsx3
+from flask import Flask, render_template, request
+
+app_id = '34U93P-WGR36VG7WE'
+client = wolframalpha.Client(app_id)
+app = Flask(__name__)
+app.static_folder = 'static'
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    res = client.query(userText)
+    answer = next(res.results).text
+    engine = pyttsx3.init()
+    engine.say(answer)
+    engine.runAndWait()
+    return str(answer)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
